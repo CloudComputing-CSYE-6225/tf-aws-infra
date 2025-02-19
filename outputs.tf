7ed656a2943e27e4cc71fd44a244e7d247f077e6
aaditshah18 <157270034+aaditshah18@users.noreply.github.com>
2025-02-19 18:00:57 -0500

    Fix terraform code formatting and validation (#3)

--- outputs.tf
+++ outputs.tf
@@ -12,18 +12,3 @@ output "private_subnet_ids" {
   description = "List of IDs for the private subnets"
   value       = module.subnets.private_subnet_ids
 }
-
-output "internet_gateway_id" {
-  description = "ID of the Internet Gateway"
-  value       = module.networking.internet_gateway_id
-}
-
-output "public_route_table_id" {
-  description = "ID of the public route table"
-  value       = module.networking.public_route_table_id
-}
-
-output "private_route_table_id" {
-  description = "ID of the private route table"
-  value       = module.networking.private_route_table_id
-}
