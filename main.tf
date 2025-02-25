ed6e6ee03b99ed8acd50c2b2281130b8aecc9dd1
Aadit <shah.aadit1@northeastern.edu>
2025-02-24 19:37:16 -0500

    Check that the CI fails

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "internet_gateway" {
 module "subnets" {
   source = "./modules/subnets"
 
-  vpc_id               = module.vpc.vpc_id
+  vpc_id               = module.avpc.vpc_id
   environment          = var.environment
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
