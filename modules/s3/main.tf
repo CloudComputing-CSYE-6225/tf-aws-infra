5881da5f7bf9e3a8435b13cdd65a467387f8e8e4
Aadit <shah.aadit1@northeastern.edu>
2025-03-16 23:45:29 -0400

    Removed outputs from s3 main.tf file

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -37,11 +37,3 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
     }
   }
 }
-
-output "bucket_name" {
-  value = aws_s3_bucket.assignment_bucket.bucket
-}
-
-output "bucket_arn" {
-  value = aws_s3_bucket.assignment_bucket.arn
-}
