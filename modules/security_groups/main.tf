d05eb6daa0d0c35b84874485ad3878f73975ca0c
Aadit <shah.aadit1@northeastern.edu>
2025-03-30 22:17:18 -0400

    Security group fix

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -3,96 +3,12 @@ resource "aws_security_group" "main" {
   description = "Security group for web application instances"
   vpc_id      = var.vpc_id
 
-  # SSH access
+  # SSH access from specific IP ranges
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow SSH access from any IP"
-  }
-
-  # HTTP access
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow HTTP traffic from any IP"
-  }
-
-  # HTTPS access
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow HTTPS traffic from any IP"
-  }
-
-  ingress {
-    from_port   = var.application_port
-    to_port     = var.application_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow application traffic on port ${var.application_port} from any IP"
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
-  }
-
-  tags = {
-    Name = "application-security-group"
-  }
-}
-
-# Add new DB security group
-resource "aws_security_group" "db" {
-  name        = "${var.environment}-db-sg"
-  description = "Security group for database instances"
-  vpc_id      = var.vpc_id
-
-  # Database access from application security group
-  ingress {
-    from_port       = var.db_port
-    to_port         = var.db_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.main.id]
-    description     = "Allow database traffic from application security group"
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
-  }
-
-  tags = {
-    Name = "database-security-group"
-  }
-}
-
-
-resource "aws_security_group" "main" {
-  name        = "${var.environment}-sg"
-  description = "Security group for web application instances"
-  vpc_id      = var.vpc_id
-
-  # SSH access from specific IP ranges (you might want to restrict this further)
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Consider restricting this to your own IP range
     description = "Allow SSH access from specified IP ranges"
   }
 
