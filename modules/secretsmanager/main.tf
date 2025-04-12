1558e9fce4beae985ad90b317ae0033211bc78dc
Aadit <shah.aadit1@northeastern.edu>
2025-04-12 13:53:38 -0400

    Secret names simplified

--- modules/secretsmanager/main.tf
+++ modules/secretsmanager/main.tf
@@ -1,16 +1,8 @@
 resource "aws_secretsmanager_secret" "secret" {
-  name                    = "${var.environment}-db-credentials-${formatdate("YYYYMMDD", timestamp())}"
+  name                    = var.secret_name
   description             = var.description
   kms_key_id              = var.kms_key_id
   recovery_window_in_days = var.recovery_window_in_days
-
-  lifecycle {
-    ignore_changes = [
-      # Prevent terraform from trying to modify these after creation
-      kms_key_id,
-      name
-    ]
-  }
 
   tags = merge(
     {
@@ -19,13 +11,4 @@ resource "aws_secretsmanager_secret" "secret" {
     },
     var.tags
   )
-}
-
-resource "aws_secretsmanager_secret_version" "secret_version" {
-  secret_id     = aws_secretsmanager_secret.secret.id
-  secret_string = var.secret_string
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
